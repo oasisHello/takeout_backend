2585fb3ce7ef8ee96a3f40e0fcd030c3d3258055
oasishalo <gongstats@gmail.com>
2024-12-13 18:35:53 +0900

    Initialization

---
+++ takeout_backend-common/src/main/java/oasis/takeout/common/exception/DemoModeException.java
@@ -0,0 +1,15 @@
+package oasis.takeout.common.exception;
+
+/**
+ * 演示模式异常
+ * 
+ * @author ruoyi
+ */
+public class DemoModeException extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    public DemoModeException()
+    {
+    }
+}
---
+++ takeout_backend-quartz/src/main/java/oasis/takeout/quartz/task/MyTask.java
@@ -0,0 +1,13 @@
+package oasis.takeout.quartz.task;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyTask {
+
+	public void showTime() {
+		System.out.println("【oasis】Scheduling task starting: "+new Date());
+	}
+}
